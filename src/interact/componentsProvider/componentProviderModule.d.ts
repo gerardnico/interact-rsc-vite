d91c7843526bf07bca67f3b53a8173be0ce44a4f
Nicolas GERARD <gerardnico@gmail.com>
2026-03-12 12:20:11 +0100

    feat: added component to markdown processing

--- src/interact/componentsProvider/componentProviderModule.d.ts
+++ src/interact/componentsProvider/componentProviderModule.d.ts
@@ -1,12 +1,13 @@
-// noinspection JSUnusedGlobalSymbols
-declare module 'interact:components' {
 
+declare module 'interact:components' {
+    import type {MDXComponents} from "mdx/types.js";
     import type {ComponentType} from "react";
     import type {CodeProps} from "#components/Code";
     import type {TemplateProps} from "./templateComponent.js";
     import type {PageModule} from "../pages/pageModule.js";
 
     export function getLayoutComponent(name: string): ComponentType<TemplateProps> | undefined
+    export function useMDXComponents(): MDXComponents
     export const Code: ComponentType<CodeProps>
     export const NotFound: PageModule
 
--- src/interact/markdown/processing/interactMd.ts
+++ src/interact/markdown/processing/interactMd.ts
@@ -4,12 +4,13 @@ import type {Root} from "mdast";
 import YAML from "yaml";
 import remarkRehype from "remark-rehype";
 import rehypeReact from "rehype-react";
-import {type Component, createElement, Fragment} from "react";
+import {createElement, Fragment} from "react";
 import {jsx, jsxs} from "react/jsx-runtime";
 import {getMandatoryUnifiedPlugins} from "../conf/markdownBasePlugins.js";
 import {markdownConfig} from "@interact/markdown-config";
 import interactConfig from "interact:config";
 import type {VFile} from "vfile";
+import {useMDXComponents} from "interact:components";
 
 const interactMd = {
     process: async (content: string | VFile) => {
@@ -20,7 +21,7 @@ const interactMd = {
         if (process.env['NODE_ENV'] !== "production") {
             strictYamlParsing = true
         }
-        let components: Component[] = []
+        let components = useMDXComponents()
         // We installed rehypeReact because Mdx use rehypeRecma as compiler (ie the hast goes to the JavaScript Tree)
         // https://github.com/mdx-js/mdx/blob/af23c2d18b58467db567b7afe78d7492bb4ea4bc/packages/mdx/lib/core.js#L161
         let frontmatter = {}
